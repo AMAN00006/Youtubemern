01b8ff77d8c2fce75be599272718bfa1def26db0
AMAN00006 <147026837+AMAN00006@users.noreply.github.com>
2023-10-05 11:40:24 +0530

    Update app.js

--- YT-Server/app.js
+++ YT-Server/app.js
@@ -52,22 +52,23 @@ mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-const connect = () => {
-    mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("Connected to db");
-        })
-        .catch(err => {
-            console.error("Error connecting to MongoDB:", err);
-        });
-}
+// const connect = () => {
+//     mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true })
+//         .then(() => {
+//             console.log("Connected to db");
+//         })
+//         .catch(err => {
+//             console.error("Error connecting to MongoDB:", err);
+//         });
+// }
 
 
+mongoose.connect('mongodb+srv://aman:aman9616223392@cluster0.rr10twt.mongodb.net/Youtube?retryWrites=true&w=majority')
 
-app.listen(4004, () => {
-    connect(),
-        console.log("Connected at 4004 port")
-})
+// app.listen(4004, () => {
+//     connect(),
+//         console.log("Connected at 4004 port")
+// })
 
 
 const corsOptions = {
