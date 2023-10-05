d82e7a4340404ec22fea92b13e989da178724081
AMAN00006 <147026837+AMAN00006@users.noreply.github.com>
2023-10-05 14:33:03 +0530

    Update app.js

--- YT-Server/app.js
+++ YT-Server/app.js
@@ -55,9 +55,15 @@ dotenv.config();
 //         // credentials: true,
 //       }),
 //     );
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.options('*',cors())
-
-
 app.use(cors())
 
 
